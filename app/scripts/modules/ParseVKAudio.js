506179e031307382f5f2ae644728cd9d5adf88cb
Ruslan Shevcov <sasa02sasa@mail.ru>
2021-03-24 23:32:46 +0600

    Добавлено получение общего количество минут, секунд и формирование время

--- app/scripts/modules/ParseVKAudio.js
+++ app/scripts/modules/ParseVKAudio.js
@@ -2,7 +2,10 @@ export default class {
   constructor({element}) {
     this.element = element;
     this.audio = [];
-    this.time = null;
+    this.time = {
+      minutes: 0,
+      seconds: 0,
+    };
     this.init();
   }
   getAudio() {
@@ -10,8 +13,29 @@ export default class {
     audios.forEach(audio => {
       const name = audio.querySelector('.audio_row__title_inner').textContent;
       const time = audio.querySelector('.audio_row__duration').textContent
-      console.log(`${name} - ${time}`);
+      this.audio.push({
+        name,
+        time,
+      })
+      const arrayTime = time.split(":");
+      this.time.minutes += +arrayTime[0];
+      this.time.seconds += +arrayTime[1] + arrayTime[0] * 60;
     })
+    console.table(this.audio);
+    console.log(`Общее количество минут:${this.time.minutes}, Общее количество секунд:${this.time.seconds}`);
+    const {minute, seconds} = this.getMinuteFromSeconds(this.time.seconds);
+    const {hour, minutes} = this.getHoursFromMinute(minute);
+    console.log(`${hour}:${minutes}:${seconds}`);
+  }
+  getMinuteFromSeconds(sec) {
+    let minute = Math.floor(sec / 60);
+    let seconds = sec - (minute * 60);
+    return {minute, seconds}
+  }
+  getHoursFromMinute(min) {
+    let hour = Math.floor(min / 60);
+    let minutes = min - (hour * 60);
+    return {hour, minutes}
   }
   init(){
     this.getAudio();
