aca59078ced02cf14c6b4bbeaa906697d07af957
Ruslan Shevcov <sasa02sasa@mail.ru>
2021-03-24 22:36:18 +0600

    Удалены ненужные изображения и файлы

--- app/scripts/modules/test.js
+++
@@ -1,9 +0,0 @@
-function test() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve('TEST async/await');
-        }, 1000);
-    })
-}
-
-export default test;
