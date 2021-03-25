04a8f863c620f3311adf0cec5979d3ad7d8f1737
Ruslan Shevcov <sasa02sasa@mail.ru>
2021-03-25 15:10:01 +0600

    Добавление автовыводы полученной информации и добавление комментариев к коду

--- app/scripts/modules/ParseVKAudio.js
+++ app/scripts/modules/ParseVKAudio.js
@@ -1,22 +1,24 @@
 export default class {
   constructor({ element }) {
-    this.element = element;
-    this.audio = [];
+    this.element = element;//Родительский элементы
+    this.audio = [];//Массив всех аудио
     this.time = {
-      minutes: 0,
       seconds: 0,
     };
-    this.observer = null;
-    this.height = window.innerHeight;
-    this.init();
+    this.observer = null;//Обсервер
+    this.timeout = null;//Таймер для вывода собранной информации
+    this.init();//Инициализация
   }
   getAudio(params) {
     if (params?.length <= 100) {
+      clearTimeout(this.timeout);
       this.parseAudio(params);
       console.log("MAKE OBSERVE!");
+      this.timeout = setTimeout(() => { this.getInformation() }, 500);
       window.scrollBy(0, 10000);
     }
   }
+  //Функция для парсинга треков, в которую передаем элемент, откуда нужно брать треки
   parseAudio(elements) {
     elements.forEach((element) => {
       const audio = element?.addedNodes ? element.addedNodes[0] : element;
@@ -27,39 +29,50 @@ export default class {
           name,
           time,
         });
-        const arrayTime = time.split(":");
-        this.time.minutes += +arrayTime[0];
-        this.time.seconds += +arrayTime[1] + arrayTime[0] * 60;
+        const arrayTime = time.split(":");//Сплитим массив времени
+        this.time.seconds += +arrayTime[1] + arrayTime[0] * 60;//Перевеодим все в секунды и записываем
       }
     });
   }
+  //Функция для получение минут из секунд
   getMinuteFromSeconds(sec) {
     let minute = Math.floor(sec / 60);
     let seconds = sec - minute * 60;
     return { minute, seconds };
   }
+  //Функция для получение часов из минут
   getHoursFromMinute(min) {
     let hour = Math.floor(min / 60);
     let minutes = min - hour * 60;
     return { hour, minutes };
   }
+   //Функция для получение дней из минут
+  getDayFromHours(hour) {
+    let day = Math.floor(hour / 24);
+    let hours = hour - day * 24;
+    return { day, hours };
+  }
   createObserve() {
-    this.observer = new MutationObserver((params) => this.getAudio(params));
+    this.observer = new MutationObserver((params) => this.getAudio(params));//Создаем обсервер и передаем колбек
     this.observer.observe(this.element, {
       childList: true, // наблюдать за непосредственными детьми
       characterDataOldValue: false, // передавать старое значение в колбэк
     });
   }
   init() {
-    this.parseAudio(this.element.querySelectorAll(".audio_row"));
-    this.createObserve();
-    window.scrollBy(0, 10000);
+    this.parseAudio(this.element.querySelectorAll(".audio_row"));//Парсим первые 100 аудио
+    this.createObserve();//Создаем обсервер
+    window.scrollBy(0, 10000);//Скроллим до новых треков, тем самым запуская обсервер
   }
+  //Функция для вывода информации.
   getInformation() {
     console.table(this.audio);
-    console.log(`Общее количество минут:${this.time.minutes}, Общее количество секунд:${this.time.seconds}`);
     const { minute, seconds } = this.getMinuteFromSeconds(this.time.seconds);
     const { hour, minutes } = this.getHoursFromMinute(minute);
-    console.log(`${hour}:${minutes}:${seconds}`);
+    const { hours, day } = this.getDayFromHours(hour);
+    console.log(`Количество аудио: ${this.audio.length}`);
+    console.log(`${minute} минут : ${seconds} секунды`);
+    console.log(`${hour} часов : ${minutes} минут : ${seconds} секунды`);
+    console.log(`${day} дней : ${hours} часов : ${minutes} минут : ${seconds} секунды`);
   }
 }
