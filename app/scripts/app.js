1865e110fe5ab629a5f8c23d6c4742246097f635
Ruslan Shevcov <sasa02sasa@mail.ru>
2021-03-25 15:15:43 +0600

    Создан билд в продакшен

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,4 @@
 import ParseVKAudio from "./modules/ParseVKAudio";
-document.addEventListener("DOMContentLoaded", () => {
-  window.parse = new ParseVKAudio({
-    element: document.querySelector("._audio_page__audio_rows_list"),
-  });
+window.parse = new ParseVKAudio({
+  element: document.querySelector("._audio_page__audio_rows_list"),
 });
