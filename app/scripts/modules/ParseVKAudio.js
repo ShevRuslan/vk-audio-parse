db3db2da1ab365c9804712ccf6a2d64cc7610de0
Ruslan Shevcov <sasa02sasa@mail.ru>
2021-03-27 19:16:52 +0600

    Вывод дней и часов только в том случае, если они есть

--- app/scripts/modules/ParseVKAudio.js
+++ app/scripts/modules/ParseVKAudio.js
@@ -1,20 +1,22 @@
 export default class {
   constructor({ element }) {
-    this.element = element;//Родительский элементы
-    this.audio = [];//Массив всех аудио
+    this.element = element; //Родительский элементы
+    this.audio = []; //Массив всех аудио
     this.time = {
       seconds: 0,
     };
-    this.observer = null;//Обсервер
-    this.timeout = null;//Таймер для вывода собранной информации
-    this.init();//Инициализация
+    this.observer = null; //Обсервер
+    this.timeout = null; //Таймер для вывода собранной информации
+    this.init(); //Инициализация
   }
   getAudio(params) {
     if (params?.length <= 100) {
       clearTimeout(this.timeout);
       this.parseAudio(params);
       console.log("MAKE OBSERVE!");
-      this.timeout = setTimeout(() => { this.getInformation() }, 500);
+      this.timeout = setTimeout(() => {
+        this.getInformation();
+      }, 500);
       window.scrollBy(0, 10000);
     }
   }
@@ -29,8 +31,8 @@ export default class {
           name,
           time,
         });
-        const arrayTime = time.split(":");//Сплитим массив времени
-        this.time.seconds += +arrayTime[1] + arrayTime[0] * 60;//Перевеодим все в секунды и записываем
+        const arrayTime = time.split(":"); //Сплитим массив времени
+        this.time.seconds += +arrayTime[1] + arrayTime[0] * 60; //Перевеодим все в секунды и записываем
       }
     });
   }
@@ -46,23 +48,26 @@ export default class {
     let minutes = min - hour * 60;
     return { hour, minutes };
   }
-   //Функция для получение дней из минут
+  //Функция для получение дней из минут
   getDayFromHours(hour) {
     let day = Math.floor(hour / 24);
     let hours = hour - day * 24;
     return { day, hours };
   }
   createObserve() {
-    this.observer = new MutationObserver((params) => this.getAudio(params));//Создаем обсервер и передаем колбек
+    this.observer = new MutationObserver((params) => this.getAudio(params)); //Создаем обсервер и передаем колбек
     this.observer.observe(this.element, {
       childList: true, // наблюдать за непосредственными детьми
       characterDataOldValue: false, // передавать старое значение в колбэк
     });
   }
   init() {
-    this.parseAudio(this.element.querySelectorAll(".audio_row"));//Парсим первые 100 аудио
-    this.createObserve();//Создаем обсервер
-    window.scrollBy(0, 10000);//Скроллим до новых треков, тем самым запуская обсервер
+    this.parseAudio(this.element.querySelectorAll(".audio_row")); //Парсим первые 100 аудио
+    this.timeout = setTimeout(() => {
+      this.getInformation();
+    }, 500);
+    this.createObserve(); //Создаем обсервер
+    window.scrollBy(0, 10000); //Скроллим до новых треков, тем самым запуская обсервер
   }
   //Функция для вывода информации.
   getInformation() {
@@ -72,7 +77,11 @@ export default class {
     const { hours, day } = this.getDayFromHours(hour);
     console.log(`Количество аудио: ${this.audio.length}`);
     console.log(`${minute} минут : ${seconds} секунды`);
-    console.log(`${hour} часов : ${minutes} минут : ${seconds} секунды`);
-    console.log(`${day} дней : ${hours} часов : ${minutes} минут : ${seconds} секунды`);
+    if (hour != 0) {
+      console.log(`${hour} часов : ${minutes} минут : ${seconds} секунды`);
+      if (day != 0) {
+        console.log(`${day} дней : ${hours} часов : ${minutes} минут : ${seconds} секунды`);
+      }
+    }
   }
 }
